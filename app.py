521d73d515e61ad8a3c1f6dd2bd6c819a00c4dcd
Maximiliano Tosca <maxitosca@gmail.com>
2020-06-09 15:52:40 -0300

    code formatting

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ import difflib
 from difflib import get_close_matches
 import psycopg2
 
-br = '\n'
+br = "\n"
+
 
 def fetch_word_definition(cursor, word):
     # handle different situations as Paris, paris or PARIS
-    word_list = (
-        word.lower(),
-        word.title(),
-        word.upper()
-    )
+    word_list = (word.lower(), word.title(), word.upper())
     fetch_sql = """select definition from definitions where word in %s"""
     cursor.execute(fetch_sql, (word_list,))
     defs = cursor.fetchall()
     return [d[0] for d in defs]
+
 
 def fetch_all_words(cursor):
     all_words_sql = "SELECT DISTINCT word FROM definitions;"
@@ -22,26 +20,28 @@ def fetch_all_words(cursor):
     words = cursor.fetchall()
     return [w[0] for w in words]
 
+
 def handle_type_error(cursor, word):
-    # handle close matches as 'rainn' to 'rain' 
+    # handle close matches as 'rainn' to 'rain'
     matches = get_close_matches(word, fetch_all_words(cur), 3, 0.8)
-    
+
     if matches:
         confirm = input(f"Did you mean '{matches[0]}' instead? Enter 'Yes' or 'No' ")
-        if confirm.lower() == 'yes':
+        if confirm.lower() == "yes":
             return define(cursor, matches[0])
-        elif confirm.lower() == 'no':
+        elif confirm.lower() == "no":
             return "The word does not exist. Please double check it."
         else:
             return "Invalid entry."
     else:
         return "The word does not exist. Please double check it."
 
+
 def define(cur, word):
     definitions = fetch_word_definition(cur, word)
     if definitions:
         return br.join(definitions)
-        
+
     return handle_type_error(cur, word)
 
 
@@ -49,7 +49,9 @@ input_word = input("Enter word: ")
 
 try:
     # connect to DB
-    conn = psycopg2.connect("dbname='english_dictionary' user='postgres' host='localhost' password='postgres'")
+    conn = psycopg2.connect(
+        "dbname='english_dictionary' user='postgres' host='localhost' password='postgres'"
+    )
     cur = conn.cursor()
 
     result = define(cur, input_word)
@@ -60,4 +62,3 @@ except (Exception, psycopg2.DatabaseError) as error:
 
 finally:
     conn.close()
-
